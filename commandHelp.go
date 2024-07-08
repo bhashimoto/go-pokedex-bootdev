0430d08ebc783314f93d9bf071cd33a416d84d13
bhashimoto <brunothashimoto@gmail.com>
2024-07-08 14:05:55 -0300

    update help command

--- commandHelp.go
+++ commandHelp.go
@@ -4,5 +4,10 @@ import "fmt"
 
 func commandHelp(args ...string) error {
 	fmt.Printf("Usage:\n")
+	for _, v := range getCommands() {
+		fmt.Printf("\t%v\t\t%v\n",
+			v.name,
+			v.description)
+	} 
 	return nil
 }
